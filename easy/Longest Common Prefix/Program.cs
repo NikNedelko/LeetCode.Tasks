278f2c3d720113e48f7e97f81fdfe732bdae76cf
Nikita <nedelko1nik@gmail.com>
2021-12-13 09:52:14 +0300

    fix solutions and add new katas

---
+++ easy/Longest Common Prefix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+            Solution.LongestCommonPrefix(new string[] { "dog", "racecar", "car" });
+            Solution.LongestCommonPrefix(new string[] { "flow", "flower", "flowight" });
+            Solution.LongestCommonPrefix(new string[] { "c", "acc", "ccc" });
+            Solution.LongestCommonPrefix(new string[] { "flow", "", "flowight" });
+        }
+    }
+
+    public class Solution
+    {
+        //second sol
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            int length = strs.Length;
+            if (length == 0) return "";
+            if (length == 1) return strs[0];
+
+            int count = strs[0].Length;
+            string answer = strs[0];
+            for (int i = 1; i < length; i++)
+            {
+                int strsLength = strs[i].Length;
+                for (int j = 0; j < count; j++)
+                {
+
+                    if (j == strsLength || strs[i][j] != answer[j])
+                    {
+                        answer = answer.Substring(0, j);
+                        count = answer.Length;
+                        if (count == 0) return "";
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ easy/Roman to Integer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Roman_to_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.RomanToInt("III");
+            Solution.RomanToInt("LVIII");
+            Solution.RomanToInt("IV");
+            Solution.RomanToInt("IX");
+        }
+    }
+
+    public class Solution
+    {
+        private static Dictionary<char, int> RomanDictionary = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+        public static int RomanToInt(string s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += RomanDictionary[s[i]];
+            }
+            if (s.Contains("IV") || s.Contains("IX"))
+                result -= 2;
+            if (s.Contains("XL") || s.Contains("XC"))
+                result -= 20;
+            if (s.Contains("CD") || s.Contains("CM"))
+                result -= 200;
+            if (result > 3999)
+                return 0;
+            return result;
+        }
+
+        public static int SecondRomanToInt(string s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && RomanDictionary[s[i]] < RomanDictionary[s[i + 1]])
+                {
+                    result -= RomanDictionary[s[i]];
+                }
+                else
+                {
+                    result += RomanDictionary[s[i]];
+                }
+            }
+            return result;
+        }
+    }
+}
