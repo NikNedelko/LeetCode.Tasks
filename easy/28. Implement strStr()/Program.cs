a1426c6df8f6ce15149d55a1b2cd7f6bbadea6a8
Nikita <nedelko1nik@gmail.com>
2021-12-14 10:53:34 +0300

    morning katas

---
+++ easy/28. Implement strStr()/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _28._Implement_strStr__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.StrStr("hello", "l");
+            Solution.StrStr("aaaaa", "bba");
+            Solution.StrStr("a", "a");
+        }
+    }
+
+    public class Solution
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack == "" && needle == "" || needle == "")
+                return 0;
+            if (haystack == needle)
+                return 0;
+
+            int result = haystack.IndexOf(needle);
+            return result;
+        }
+    }
+}
---
+++ easy/35. Search Insert Position/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _35._Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.SearchInsert(new int[] { 1, 3, 5, 6 }, 5);
+            Solution.SearchInsert(new int[] { 1, 2 }, 3);
+            Solution.SearchInsert(new int[] { 1 }, 0);
+            Solution.SearchInsert(new int[] { 1, 3}, 1);
+            Solution.SearchInsert(new int[] { 1,3,5,6 }, 7);
+        }
+    }
+
+    public class Solution
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+                try
+                {
+                    if (nums[i] < target && target <= nums[i + 1])
+                        return i + 1;
+                }
+                catch
+                {
+                    return i+1;
+                }
+            }
+            return 0;
+        }
+    }
+}
