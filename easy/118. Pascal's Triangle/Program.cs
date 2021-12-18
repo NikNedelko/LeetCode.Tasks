6e67ff8781e643a05aefcb3f84dad6f8faf6572f
Nikita <nedelko1nik@gmail.com>
2021-12-18 22:31:36 +0300

    new kata every day

---
+++ easy/118. Pascal's Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _118._Pascal_s_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.Generate(30);
+            Solution.Generate(2);
+            Solution.Generate(3);
+            Solution.Generate(4);
+            Solution.Generate(5);
+        }
+    }
+
+    public class Solution
+    {
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            for (int i = 0; i < numRows; i++)
+            {
+                IList<int> builder = new List<int>();
+                for (int c = 0; c <= i; c++)
+                {
+                    builder.Add(Convert.ToInt32( factorial(i) / (factorial(c) * factorial(i - c)))); 
+                }
+                result.Add(builder);
+                //новая строка
+            }
+            return result;
+        }
+         private static long factorial(int n)
+        {
+            long i, x = 1;
+            for (i = 1; i <= n; i++)
+            {
+                x *= i;
+            }
+            return x;
+        }
+    }
+}
+
---
+++ easy/88. Merge Sorted Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace _88._Merge_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = new int[] { 1, 2, 3, 0, 0, 0 };
+            Solution.Merge(result, 3, new int[] { 2, 5, 6 }, 3);
+            Solution.Merge(new int[] { 1 }, 0, new int[] { }, 1);
+            Solution.Merge(new int[] { }, 0, new int[] { 1 }, 1);
+            //  Solution.Merge(new int[]{}, ,new int[]{},);
+            //Solution.Merge(new int[]{}, ,new int[]{},);
+        }
+    }
+
+    public class Solution
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            while (m > 0 && n > 0)
+            {
+                if (nums1[m - 1] > nums2[n - 1])
+                {
+                    nums1[m + n - 1] = nums1[m - 1];
+                    m--;
+                }
+                else
+                {
+                    nums1[m + n - 1] = nums2[n - 1];
+                    n--;
+                }
+            }
+            while (n > 0)
+            {
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
+            }
+
+        }
+    }
+}
