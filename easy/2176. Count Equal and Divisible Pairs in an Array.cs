05f138e3f9cad77a6e0a5276800e222fd333c7b0
Nik Nedelko <nedelko1nik@gmail.com>
2022-12-02 11:43:32 +0100

    2176. Count Equal and Divisible Pairs in an Array

---
+++ easy/2176. Count Equal and Divisible Pairs in an Array.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int CountPairs(int[] nums, int k) {
+        var count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i+1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i * j % k == 0) count++;
+            }
+        }
+
+        return count;
+    }
+}
