74fd856d1c002fff9ade03ec77710366787bb7e2
Nik Nedelko <nedelko1nik@gmail.com>
2022-12-24 14:36:04 +0100

    1475. Final Prices With a Special Discount in a Shop

---
+++ easy/1475. Final Prices With a Special Discount in a Shop.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices) {
+        
+        for(int i = 0; i < prices.Length; i++){
+            for(int j = i + 1; j < prices.Length; j++){
+                if(prices[j] <= prices[i])
+                {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return prices;
+    }
+}
