7a37b75a178e20e3569fd82bd75c7968e066b16a
Nik Nedelko <nedelko1nik@gmail.com>
2025-01-02 14:18:08 +0100

    3162. Find the Number of Good Pairs I

---
+++ easy/3162. Find the Number of Good Pairs I.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int NumberOfPairs(int[] nums1, int[] nums2, int k) {
+        var result = 0;
+
+        for(var i = 0; i < nums1.Length; i++){
+            for(var j = 0; j < nums2.Length; j++){
+                if(nums1[i] % (nums2[j] * k) == 0){
+                        result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
